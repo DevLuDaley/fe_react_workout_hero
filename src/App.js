233c223151cc5537edc2f9043355471c5fd5c391
devludaley <daleyluis@gmail.com>
2020-12-25 22:00:05 -0500

    import <MainContainer />

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routines from './components/RoutinesList'
+import MainContainer from './containers/MainContainer'
+import RoutinesContainer from './containers/RoutinesContainer'
+
+
 
 function App() {
   return (
     <Fragment>
       
     <div className="App">
-      <header className="App-header">        
+      <header className="App-header">
       </header>
+      <MainContainer/>
     </div>
     </Fragment>
   );
