df0588e85a8b5da326f9bc078b91d9693eaa71f2
devludaley <daleyluis@gmail.com>
2020-12-25 21:59:22 -0500

    create routinesReducer with CRUD

---
+++ src/reducers/routinesReducer.js
@@ -0,0 +1,38 @@
+const intialState = {routines: []}
+
+export default function routinesReducer(state = intialState, action) {
+// debugger;
+    switch (action.type) {
+        case 'FETCH_ROUTINES':        // debugger;
+            return {routines: action.payload}
+
+        // case 'ADD_ROUTINE':
+        //     let newRoutine = action.payload
+        //     return {...state.routines, routines: state.routines.concat(newRoutine)}
+
+        // case 'UPDATE_ROUTINE':
+        //     const payloadId = action.payload.id
+        //     return {routines: state.routines.map(routine => routine.id == payloadId ? action.payload : routine)}
+        //         // return Object.assign({}, state, {routines: currentRoutines, routine: currentRoutine})
+        // case 'DELETE_ROUTINE':
+        //     const filteredRoutines = state.routines.filter(routine => routine.id !== action.payload);
+        //     return {...state, routines: filteredRoutines}
+
+        // case 'ADD_ROUTINE_RECIPE':
+            
+        //     let routineRecipes =  state.routines.map(routine => {
+        //         if (routine.id == action.payload.id) {
+        //             return action.payload
+        //             } else {
+        //                 return routine
+        //             }
+        //           }
+        //         )
+        //     return {...state, routines: routineRecipes}
+
+        default:
+            // return {...state}
+            return state
+    }
+
+}
