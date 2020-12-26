a1dc8386c05970f305193d6401e6c721dff88bbf
devludaley <daleyluis@gmail.com>
2020-12-25 21:59:08 -0500

    create main rootReducer and combineReducers

---
+++ src/reducers/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import routinesReducer from './routinesReducer'
+
+const rootReducer = combineReducers({
+    routinesReducer
+})
+
+export default rootReducer
