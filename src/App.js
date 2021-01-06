02e0ef05d5834e93b8e2cc335c4a23e691a6f6b9
devludaley <daleyluis@gmail.com>
2021-01-05 21:59:00 -0500

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, {Fragment} from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import Routines from './components/RoutinesList'
 import MainContainer from './containers/MainContainer'
-// import RoutinesContainer from './containers/RoutinesContainer'
-
-
 
 function App() {
   return (
     <Fragment>
-      
     <div className="App container">
       <header className="App-header">
       </header>
-      <MainContainer id="main-container" />
+      <MainContainer id="main-container"/>
     </div>
     </Fragment>
   );
