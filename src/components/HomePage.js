9d349dc26b71ec936862e1a3f14d34c9f3d1ad59
devludaley <daleyluis@gmail.com>
2021-01-05 21:59:15 -0500

    Create HomePage.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <p>they call me HomePage</p>
+          );
+    }
+}
+ 
+export default HomePage;
