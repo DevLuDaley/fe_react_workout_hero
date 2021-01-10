1dbdba1757d905d89fcf0fa35feff011858553cf
devludaley <daleyluis@gmail.com>
2021-01-10 18:17:04 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: { presets: ["@babel/env"] }
             },
             {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http://localhost:3000/dist/'
+        // ,
+        // hot: true,
+        // hotOnly: true
+    },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
+        new webpack.HotModuleReplacementPlugin(), 
+        new HtmlWebpackPlugin({
+         template: path.resolve( __dirname, 'public/index.html' ),
+         filename: 'index.html'
+      }), new webpack.NoEmitOnErrorsPlugin()
+]
 };
