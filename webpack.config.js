606139ec3145d1cad5a98d035580e561731d84fa
devludaley <daleyluis@gmail.com>
2021-01-10 18:49:09 -0500

    remove url-loader from webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,6 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader'
             }
         ]
     },
