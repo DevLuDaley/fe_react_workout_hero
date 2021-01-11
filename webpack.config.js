0f37e599c84369bdffa649f32b3030f8f331f5f2
devludaley <daleyluis@gmail.com>
2021-01-10 23:03:43 -0500

    remove publicPath from webpack.config.js && rerun build

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,6 @@ module.exports = {
     resolve: { extensions: ['*', '.js', '.jsx'] },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
         filename: 'bundle.js'
     },
     devServer: {
