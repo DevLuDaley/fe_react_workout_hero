76fca2f71616ee0dc956f4e9be8dfa8a19c975c6
devludaley <daleyluis@gmail.com>
2021-01-10 21:31:50 -0500

    prepend '.' to '/dist/'

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     resolve: { extensions: ['*', '.js', '.jsx'] },
     output: {
         path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/dist/',
+        publicPath: './dist/',
         filename: 'bundle.js'
     },
     devServer: {
