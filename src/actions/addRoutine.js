513bc095477e07a547f60b9ca16876944ad3736b
devludaley <daleyluis@gmail.com>
2021-01-13 14:03:27 -0500

    addRoutine => bugfix - update fetch-api call

--- src/actions/addRoutine.js
+++ src/actions/addRoutine.js
@@ -5,7 +5,7 @@ export const addRoutine = (data) => {
 
     return(dispatch) => {
 // fetch('http://localhost:3000/api/v1/routines', {
-fetch('https://be-workout-hero2.herokuapp.com/api/v1/routines/', {
+fetch('https://be-workout-hero2.herokuapp.com/api/v1/routines', {
     headers: {
         'Content-Type' : 'application/json',
         'Accept' : 'application/json'
