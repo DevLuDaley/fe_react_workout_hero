9f963d094b1db3fb051d46b5f0dd52c00de8e422
devludaley <daleyluis@gmail.com>
2021-01-13 14:09:50 -0500

    routinesReducer => +console.log call to debug NewRoutineForm/>

--- src/reducers/routinesReducer.js
+++ src/reducers/routinesReducer.js
@@ -1,8 +1,8 @@
 const intialState = {routines: []}
 
 export default function routinesReducer(state = intialState, action) {
+// console.log('🚀 ~ file: routinesReducer.js ~ line 4 ~ routinesReducer ~ action', action);
 // debugger;
-console.log(action);
     switch (action.type) {
         case 'FETCH_ROUTINES':        // debugger;
             return {routines: action.payload}
