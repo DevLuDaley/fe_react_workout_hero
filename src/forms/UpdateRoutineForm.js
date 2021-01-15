58c2f8e0e8ea2eebb8269cf23386375d8d8aed37
devludaley <daleyluis@gmail.com>
2021-01-15 17:36:12 -0500

    UpdateRoutineForm => cleanup unesed code

--- src/forms/UpdateRoutineForm.js
+++ src/forms/UpdateRoutineForm.js
@@ -10,23 +10,22 @@ import {routinesReducer} from '../reducers/routinesReducer'
 class UpdateRoutineForm extends Component {
     constructor(props) {
     super(props)
-    // this.routines = this.props.routines
-    // this.id = parseInt(window.location.href.replace("http://localhost:3001/routines/", ""))
-    // this.id = parseInt(window.location.href.replace("http://localhost:3003/routines/", ""))
     this.id = parseInt(window.location.href.includes('heroku')) ?
-        parseInt(window.location.href.replace("https://fe-workout-hero.herokuapp.com/routines/", ""))
+    parseInt(window.location.href.replace("https://fe-workout-hero.herokuapp.com/routines/", ""))
        :
-        parseInt(window.location.href.replace("http://localhost:3003/routines/", ""))
-    this.routine = this.props.routines.find(routine => routine.id == this.id)
-    // this.reducer = routinesReducer
+       parseInt(window.location.href.replace("http://localhost:3003/routines/", ""))
+       this.routine = this.props.routines.find(routine => routine.id == this.id)
+       // this.routines = this.props.routines
+       // this.id = parseInt(window.location.href.replace("http://localhost:3001/routines/", ""))
+       // this.id = parseInt(window.location.href.replace("http://localhost:3003/routines/", ""))
 
     this.state = {
+        id: this.id,
+        routine_name: ""
         // routine:this.routine
         // id: parseInt(window.location.href.replace("http://localhost:3001/routines/", "")),
         // id: this.props.routineToUpdate.id,
-        id: this.id,
         // routine_name: this.props.routineToUpdate.routine_name,
-        routine_name: ""
         // routine_name: this.routine.routine_name
         }
         // this.loadRoutineInfo = this.loadRoutineInfo.bind(this)
@@ -55,51 +54,13 @@ class UpdateRoutineForm extends Component {
             })
         }
 
-
         handleSubmit = (e) => {
         e.preventDefault()
-        // debugger
-        // console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 87 ~ UpdateRoutineForm ~ this.state', this.state);
         this.props.updateRoutine(this.state)
-        // console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 87 ~ UpdateRoutineForm ~ this.state---after submitted to <updateRoutine/>', this.state);
-        // alert(`${[this.state.name]} created!`)
-
-        // this.selectedRoutine = {}
-        // console.log("YO LINE 67 this.routinesReducer data", this.routinesReducer.state);
-        // console.log("YO LINE 67 this.reducer data", this.reducer);
-        // console.log("YO LINE 67 this.reducer data", this.reducer.state);
-        // console.log("YO LINE 67 this.routinesReducer data", this.routinesReducer.case);
-        // console.log("YO LINE 68 this.routineToUpdate", this.props.routineToUpdate);
-        this.setState({
-            // routine_name: '',
-            routine_name: ''
-        })
+        this.setState({ routine_name: '' })
         }
-        // handlePlaceholder = (e) => {
-        // // console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 78 ~ UpdateRoutineForm ~ e', e)
-        //     // this.state.routine_name
-        //     // "WORDS"
-        //     // !this.state.routine_name ? this.state.routine_name : this.routine.routine_name
-        //     // console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 80 ~ UpdateRoutineForm ~ this.routine.routine_name FINALLY', this.routine.routine_name);
-        //     // if ( this.routine.routine_name && this.routine.routine_name !== this.state.routine_name) {
-        //         // return
-        //         // console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 81 ~ UpdateRoutineForm ~ this.state.routine_name', this.state.routine_name);
-        //         // this.state.routine_name
-        //     // }
 
-        //     // else {
-        //     // return
-        //     // this.routine.routine_name
-        //         // {console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 81 ~ UpdateRoutineForm ~ this.routine.routine_name', this.routine.routine_name)
-        //     // }
-        //         // this.state.routine_name
-
-        //         // {this.routine.routine_name}
-        //     }
-
-// export default connect(mapStateToProps)(App);
-    render() {
-        // this.loadRoutines
+        render() {
         return (
             <Fragment>
                 {/* {console.log("THIS.PROPS", this.props)} */}
@@ -130,15 +91,12 @@ class UpdateRoutineForm extends Component {
 }
 
 function mapStateToProps(state) {
-//   const { routines } = state
   const routineId = parseInt(window.location.href.replace("http://localhost:3003/routines/", ""))
-//   console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 143 ~ UpdateRoutineForm ~ mapStateToProps ~ routineId', routineId);
   const currentRoutine = state.routinesReducer.routines.filter(routine => routine.id == routineId)
-//   console.log('🚀 ~ file: UpdateRoutineForm.js ~ line 146 ~ UpdateRoutineForm ~ mapStateToProps ~ state.routinesReducer.routines.filter(routine => routine.id == routineId)', currentRoutine[0]);
+
   return { routines: state.routinesReducer.routines,
            routine: currentRoutine[0]
  }
 }
-
 
 export default connect(mapStateToProps, {updateRoutine})(UpdateRoutineForm);
