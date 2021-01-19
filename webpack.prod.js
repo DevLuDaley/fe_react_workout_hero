8a2410696a24b0db617cd253c925201f6324ed89
devludaley <daleyluis@gmail.com>
2021-01-18 21:45:56 -0500

    webpack.prod.js => config plugins and optimization

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,40 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
    mode: 'production',
    devtool: 'source-map',
+   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          context: path.resolve(__dirname, "dist"),
+          from: "*.html",
+          // from: "./src/*.html",
+        },
+      ],
+    }),
+  ],
+  optimization: {
+    // minimize: true,
+    minimize: false,
+    minimizer: [
+      // new HtmlMinimizerPlugin(),
+      // new HtmlMinimizerPlugin({
+      //   minimizerOptions: {
+      //     collapseWhitespace: true,
+      //   },
+      //   minify: (data, minimizerOptions) => {
+      //     const htmlMinifier = require("html-minifier-terser");
+      //     const [[filename, input]] = Object.entries(data);
+
+      //     return htmlMinifier.minify(input, minimizerOptions);
+      //   },
+      // }),
+        new HtmlMinimizerPlugin(),
+    ],
+  }
 });
