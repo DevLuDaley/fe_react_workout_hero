4173c7188e9c907cf4dda3478b3255655e4fc269
devludaley <daleyluis@gmail.com>
2021-01-18 23:07:27 -0500

    webpack.prod.js => plugins => comment out because duplicate logic works in webpack.common

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,34 +7,34 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = merge(common, {
    mode: 'production',
    devtool: 'source-map',
-   plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          context: path.resolve(__dirname, "dist"),
-          from: "*.html",
-          // from: "./src/*.html",
-        },
-      ],
-    }),
-  ],
-  optimization: {
-    // minimize: true,
-    minimize: false,
-    minimizer: [
-      new HtmlMinimizerPlugin(),
-      new HtmlMinimizerPlugin({
-        minimizerOptions: {
-          collapseWhitespace: true,
-        },
-        minify: (data, minimizerOptions) => {
-          const htmlMinifier = require("html-minifier-terser");
-          const [[filename, input]] = Object.entries(data);
+  //  plugins: [
+  //   new CopyPlugin({
+  //     patterns: [
+  //       {
+  //         context: path.resolve(__dirname, "dist"),
+  //         from: "*.html",
+  //         // from: "./src/*.html",
+  //       },
+  //     ],
+  //   }),
+  // ],
+  // optimization: {
+  //   // minimize: true,
+  //   minimize: false,
+  //   minimizer: [
+  //     // new HtmlMinimizerPlugin(),
+  //     new HtmlMinimizerPlugin({
+  //       minimizerOptions: {
+  //         collapseWhitespace: true,
+  //       },
+  //       minify: (data, minimizerOptions) => {
+  //         const htmlMinifier = require("html-minifier-terser");
+  //         const [[filename, input]] = Object.entries(data);
 
-          return htmlMinifier.minify(input, minimizerOptions);
-        },
-      }),
-        // new HtmlMinimizerPlugin(),
-    ],
-  }
+  //         return htmlMinifier.minify(input, minimizerOptions);
+  //       },
+  //     }),
+  //       // new HtmlMinimizerPlugin(),
+  //   ],
+  // }
 });
