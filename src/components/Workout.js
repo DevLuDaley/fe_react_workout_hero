486480c377fc781b25b400220f7b7f5341684cd7
devludaley <daleyluis@gmail.com>
2021-01-25 12:14:44 -0500

    src/components/Workout.js => display workout data

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -7,23 +7,46 @@ import { Switch, Link } from 'react-router-dom'
 // import NewWorkoutWorkoutForm from '../forms/NewWorkoutWorkoutForm'
 
 const Workout = (props) => {
-console.log('🚀 ~ file: Workout.js ~ line 10 ~ Workout ~ props', props);
-console.log('🚀 ~ file: Workout.js ~ line 11 ~ Workout ~ props', props);
+console.log('🚀 ~ file: Workout.js ~ line 10 ~ Workout ~ WORKOUT props', props);
+const {currentWorkout : workout} = props
+
+// console.log('🚀 ~ file: Workout.js ~ line 11 ~ Workout ~ props', props);
 // console.log('🚀 ~ file: Workout.js ~ line 10 ~ Workout ~ props-routine', props.current_routine);
 // console.log('🚀 ~ file: Workout.js ~ line 10 ~ Workout ~ props-routine', props.routine);
 
     return(
     <Fragment>
+    {/* {props.currentWorkout} */}
                     <Switch>
                    
                    <Link to={'/routines'}> <button> Return to Routines </button> </Link>
                     </Switch>
                    {/* <Link to={'/workouts'}> <button> Return to Workouts </button> </Link> */}
                    
+                   <div>
+                    id: {workout.id}
+                      <div key={workout.id}>
+                            {/* id: {workout.id}
+                            <br></br> */}
+                            name: {workout.workout_name}
+                            <br></br>
+                            category: {workout.workout_type}
+                            <br></br>
+                            distance: {workout.distance} 
+                            {/* ? workout.distance + " miles" : "please enter distance"}  */}
+                            <br></br>
+                            duration: {workout.duration}
+                             {/* ? workout.duration + " minutes" : "please enter duration"} */}
+                            <br></br>
+                            <br></br>
+                        </div>
+                        </div>
+
     
         </Fragment>
     )
 }
+    
     // const mapStateToProps = (state, ownProps) => {
     //     const { workouts1 } = state
     //     const { workouts } = ownProps
