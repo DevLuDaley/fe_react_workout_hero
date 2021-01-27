b588ce1bc059c3b7cf847b8ca5df043836b55d48
devludaley <daleyluis@gmail.com>
2021-01-27 09:24:08 -0500

    src/containers/RoutinesContainer.js => add fetchWorkouts

--- src/containers/RoutinesContainer.js
+++ src/containers/RoutinesContainer.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Switch} from 'react-router-dom'
 
 import {fetchRoutines} from '../actions/fetchRoutines'
+import {fetchWorkouts} from '../actions/fetchWorkouts'
 import RoutinesPage from './RoutinesPage';
 import Routine from '../components/Routine'
 // import Workout from '../components/Workout'
@@ -21,22 +22,29 @@ class RoutinesContainer extends Component {
     constructor(props) {
         super(props);
         this.fetchRoutines = fetchRoutines
-        this.state = {routines: []}
+        this.fetchWorkouts = fetchWorkouts
+        this.state = {
+            routines: [],
+            workouts: []
+        }
     }
     componentDidMount(){
         // console.log('THIS.PROPS 1', this.props)
         // this.fetchRoutines()
         this.props.fetchRoutines()
+        this.props.fetchWorkouts()
     }
     render() {
         return (
             <Fragment>
+        {console.log('🚀 ~ file: RoutinesContainer.js ~ line 46 ~ RoutinesContainer ~ componentDidMount ~ this.props', this.props)}
+        {console.log('🚀 ~ file: RoutinesContainer.js ~ line 46 ~ RoutinesContainer ~ componentDidMount ~ this.state', this.state)}
             {/* <p> Routines Container</p> */}
             {/* <Router> */}
             <Switch>
-            <Route exact path='/' render={(routerProps) => <RoutinesPage {...routerProps} routines={this.props.routines} rando="words"/>}/>
+            <Route exact path='/' render={(routerProps) => <RoutinesPage {...routerProps} routines={this.props.routines} />}/>
 
-            <Route exact path='/routines/:id' render={(routerProps) => <Routine {...routerProps} routines={this.props.routines}
+            <Route exact path='/routines/:id' render={(routerProps) => <Routine {...routerProps} routines={this.props.routines} workouts={this.props.workouts} 
              />}/>
 
             {/* <Route path='/workouts/:id' render={(routerProps) => <Workout {...routerProps} routines={this.props.routines} workouts={this.props.workouts}
@@ -44,7 +52,7 @@ class RoutinesContainer extends Component {
 
             {/* <Route exact path='/' component={HomePage}/> */}
 
-            <Route exact path='/routines' render={(routerProps) => <RoutinesPage {...routerProps} routines={this.props.routines} />}/>
+            <Route exact path='/routines' render={(routerProps) => <RoutinesPage {...routerProps} routines={this.props.routines } workouts={this.props.workouts} />}/>
 
             {/* <Route exact path='/routines/new' render={(routerProps) => <NewRoutinesContainer {...routerProps} routines={this.props.routines}/>}/> */}
 
@@ -69,9 +77,9 @@ const mapStateToProps = state => {
     return(
         {
             routines: state.routinesReducer.routines,
-            // recipes: state.recipesReducer.recipes
+            workouts: state.workoutsReducer.workouts
         }
     )
 }
  
-export default connect(mapStateToProps, {fetchRoutines})(RoutinesContainer);
+export default connect(mapStateToProps, {fetchRoutines, fetchWorkouts})(RoutinesContainer);
