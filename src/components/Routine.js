465b487189debc72c4ba22af9307f052023280aa
devludaley <daleyluis@gmail.com>
2021-02-11 19:47:42 -0500

    src/components/Routine.js => add section wrapper for css

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -37,76 +37,80 @@ const Routine = (props) => {
 
     return(
     <Fragment>
-                    <Switch>
-                   <Link to={'/routines'}> <button className='floating-nav-btns'> Return to Routines </button> </Link>
-                    </Switch>
-    {
-        props.routines ?
-        <section>
-        {/* Routine Info: */}
-        {
-            props.routines.map(routine => 
-                routine.id == props.match.params.id ? 
-        <section key={routine.id}>
-            <br></br>
-                <h1>{routine.routine_name} </h1>
-            <br></br>
-            {/* id: {routine.id} */}
-
-            {/* <p>{filtered.name}</p> */}
-    {
-        props.routines ?
-    <UpdateRoutineForm routines={props.routines} 
-            routineToUpdate={props.routines.find(routine => 
-         routine.id == props.match.params.id)}
-    />
-    : null 
-    }
-            <br></br>
-            <br></br>
-            <h1> Add a new Workout</h1>
-                {<NewRoutineWorkoutForm 
-                    routines={props.routines}
-                    currentRoutine={props.routines.find(
-                        routine => routine.id == props.match.params.id)}
-                />}
-            <br></br>
-            <br></br>
-                <h3 key={routine.id}>Workouts List: </h3> 
-                { 
-                    
-                routine.workouts.length > 0 ?
-
-                    routine.workouts.map(workout =>
-                        <div key={workout.id}>
-                            {/* id: {workout.id}
-                            <br></br> */}
-                             <Link key={"routine-url-key-" + routine.id} to={`${window.location.pathname}/workouts/${workout.id}`}>
-                                 <button className="edit-btns"> edit </button>
-                                 </Link>
-                            name: {workout.workout_name}
-                                  <button className="delete-btns" onClick={handleDeleteWorkout} id={`routine-${routine.id}-btn-delete-workout-${workout.id}`}> delete </button>
-                            <br></br>
-                            category: {workout.workout_type}
-                            <br></br>
-                            distance: {workout.distance} 
-                            {/* ? workout.distance + " miles" : "please enter distance"}  */}
-                            <br></br>
-                            duration: {workout.duration}
-                             {/* ? workout.duration + " minutes" : "please enter duration"} */}
-                            <br></br>
-                            <br></br>
-                        </div>
-                    ) : <p> No Workouts Created Yet </p>
-                    }
-                </section>
-                : null )
-        }
+    <section className="routine-detail-page">
         
-        </section>
-        :
-        'no routine here bub'
-    }
+                        <Switch>
+                       <Link to={'/routines'}> <button className='floating-nav-btns'> Return to Routines </button> </Link>
+                        </Switch>
+        {
+            props.routines ?
+            <section>
+            {/* Routine Info: */}
+            {
+                props.routines.map(routine => 
+                    routine.id == props.match.params.id ? 
+            <section key={routine.id}>
+                <br></br>
+                    <h1>{routine.routine_name} </h1>
+                <br></br>
+                {/* id: {routine.id} */}
+    
+                {/* <p>{filtered.name}</p> */}
+        {
+            props.routines ?
+        <UpdateRoutineForm routines={props.routines} 
+                routineToUpdate={props.routines.find(routine => 
+             routine.id == props.match.params.id)}
+        />
+        : null 
+        }
+                <br></br>
+                <br></br>
+                <h1> Add a new Workout</h1>
+                    {<NewRoutineWorkoutForm 
+                        routines={props.routines}
+                        currentRoutine={props.routines.find(
+                            routine => routine.id == props.match.params.id)}
+                    />}
+                <br></br>
+                <br></br>
+                    <h3 key={routine.id}>Workouts List: </h3> 
+                    { 
+                        
+                    routine.workouts.length > 0 ?
+    
+                        routine.workouts.map(workout =>
+                            <div key={workout.id}>
+                                {/* id: {workout.id}
+                                <br></br> */}
+                                 <Link key={"routine-url-key-" + routine.id} to={`${window.location.pathname}/workouts/${workout.id}`}>
+                                     <button className="edit-btns"> edit </button>
+                                     </Link>
+                                name: {workout.workout_name}
+                                      <button className="delete-btns" onClick={handleDeleteWorkout} id={`routine-${routine.id}-btn-delete-workout-${workout.id}`}> delete </button>
+                                <br></br>
+                                category: {workout.workout_type}
+                                <br></br>
+                                distance: {workout.distance} 
+                                {/* ? workout.distance + " miles" : "please enter distance"}  */}
+                                <br></br>
+                                duration: {workout.duration}
+                                 {/* ? workout.duration + " minutes" : "please enter duration"} */}
+                                <br></br>
+                                <br></br>
+                            </div>
+                        ) : <p> No Workouts Created Yet </p>
+                        }
+                    </section>
+                    : null )
+            }
+            
+            </section>
+            :
+            'no routine here bub'
+        }
+
+    </section>   
     
         </Fragment>
     )
