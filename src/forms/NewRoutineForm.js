1d23462e72102ac0964ec72fec76b9781ad14b75
devludaley <daleyluis@gmail.com>
2021-02-12 17:04:34 -0500

    add material UI form-fields

--- src/forms/NewRoutineForm.js
+++ src/forms/NewRoutineForm.js
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import {Route, Switch} from 'react-router-dom'
+import { TextField, MenuItem } from '@material-ui/core';
 
 import {addRoutine} from '../actions/addRoutine'
-// import RoutinesList from '../components/RoutinesList'
+
 
 class NewRoutineForm extends Component {
     state = {
@@ -11,52 +11,40 @@ class NewRoutineForm extends Component {
     }
 
     handleChange = (e) => {
-        // e.target.value
-        // console.log('NewRoutineForm -> e.target.value', e.target.value);
         this.setState({
             [e.target.name]: e.target.value
         })
-        // console.log('🚀 ~ file: NewRoutineForm.js ~ line 20 ~ NewRoutineForm ~ e.target.routine_name', e.target.routine_name);
-        // console.log('🚀 ~ file: NewRoutineForm.js ~ line 20 ~ NewRoutineForm ~ e.target.name', e.target.name);
-        // console.log('🚀 ~ file: NewRoutineForm.js ~ line 20 ~ NewRoutineForm ~ e.target.value', e.target.value);
-        // console.log('NewRoutineForm -> e.target.image_url', e.target.image_url);
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        // debugger
         this.props.addRoutine(this.state)
-        // alert(`${[this.state.name]} created!`)
         this.setState({ 
         name: ''
     })
     }
 
-// export default connect(mapStateToProps)(App);
     render() {
         return (
         <Fragment>
-        {/* <RoutinesList/> */}
             <form id='new-routine-form' onSubmit={this.handleSubmit}>
 
-                <label> Routine Name: </label>
-                <input type="text" placeholder='enter routine name...' value={this.state.name} name="routine_name" routine_name="blank" onChange={this.handleChange}/>
+                {/* <label> Routine Name: </label>
+                <input type="text" placeholder='enter routine name...' value={this.state.name} name="routine_name" routine_name="blank" onChange={this.handleChange}/> */}
+
+ <label> </label>
+              <TextField
+                label="Name"
+                id="margin-none"
+                value={this.state.name} name="routine_name" 
+                onChange={this.handleChange}/>
+
 
                 <button className='submit-btns' type="submit">Create Routine</button>
             </form>
-            {/* <DrillsContainer/>       */}
-            {/* <Route exact path='/routines' render={(routerProps) => <RoutinesList {...routerProps} routines={this.props.routines}/>}/> */}
-            {/* <RoutinesList/> */}
-            {/* <RoutinesList props={this.props}/> */}
         </Fragment>
         );
     }
 }
 
-
 export default connect(null, {addRoutine})(NewRoutineForm);
-// ! doesn't need mapStateToProps
-// ! doesn't need access to prop
-//  ! doesn;t need to know what is currently in store...only responsible for adding new routine and updating
-
-// ! instead of mapDispatchToProps we're calling {addRutine} and use the connect component
--- src/forms/NewRoutineWorkoutForm.js
+++ src/forms/NewRoutineWorkoutForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import { TextField, MenuItem } from '@material-ui/core';
+
 import {addRoutineWorkout} from '../actions/addRoutineWorkout'
 
 class NewRoutineWorkoutForm extends Component {
@@ -47,22 +49,96 @@ class NewRoutineWorkoutForm extends Component {
         //  }
     }
     render() { 
+              const workoutCategories = [
+                {
+                    value: 'Cardio',
+                    label: 'Cardio',
+                },
+                {
+                    value: 'Weight-Lifting',
+                    label: 'Weight-Lifting',
+                },
+                {
+                    value: 'Body-Weight',
+                    label: 'Body-Weight',
+                },
+                {
+                    value: 'Cardio-Lifting',
+                    label: 'Cardio-Lifting',
+                },
+                    ];
+
         return (  
           <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} id="new-routine-workout-form">
               {/* <input type="hidden" placeholder='' value={this.state.id} name="id" onChange={this.handleChange}/> */}
 
-            <label>Name: </label> 
-              <input type="text" placeholder='enter name...' value={this.state.workout_name} name="workout_name" onChange={this.handleChange}/>
+            {/* <label>Name: </label> 
+              <input 
+              type="text" 
+              placeholder='enter name...' 
+              value={this.state.workout_name} name="workout_name" 
+              onChange={this.handleChange}/> */}
 
-            <label>Workout Type: </label> 
-              <input type="text" placeholder='enter type...' value={this.state.workout_type} name="workout_type" onChange={this.handleChange}/>
+            <label> </label>
+              <TextField
+                label="Name"
+                id="margin-none"
+                value={this.state.workout_name} name="workout_name" 
+                onChange={this.handleChange}/>
 
-            <label> Distance: </label> 
+            {/* <label>Workout Type: </label> 
+              <input type="text" placeholder='enter type...' value={this.state.workout_type} name="workout_type" onChange={this.handleChange}/> */}
+
+            <label> </label>
+              <TextField
+                id="standard-select-currency"
+                select label="Type"
+                name="workout_type"
+                value={this.state.workout_type}
+                onChange={this.handleChange}
+            >
+            {workoutCategories.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                {option.label}
+                </MenuItem>
+            ))}
+            </TextField>
+
+
+            {/* <label> Distance: </label> 
               <input type="text" placeholder='enter distance...' value={this.state.distance} name="distance" onChange={this.handleChange}/>
               
             <label> Duration: </label> 
-              <input type="text" placeholder='enter duration...' value={this.state.duration} name="duration" onChange={this.handleChange}/>
+              <input type="text" placeholder='enter duration...' value={this.state.duration} name="duration" onChange={this.handleChange}/> */}
+<label> </label>
+                            <TextField
+                                id="outlined-number"
+                                label="Miles"
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                name="distance" 
+                                // placeholder={this.props.detail.distance} 
+                                value={this.state.distance}
+                                onChange={this.handleChange}
+                                />
+
+                            <label> </label>
+                            <TextField
+                                id="outlined-number"
+                                label="Minutes"
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                name="duration" 
+                                // placeholder={this.props.detail.duration} 
+                                value={this.state.duration}
+                                onChange={this.handleChange}
+                                />
+ 
 
                 <button className='submit-btns' type="submit">Add Workout</button>
         </form>
