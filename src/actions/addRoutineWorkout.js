f2d0b383a5b1b2b820dde755d0363fed63a676f7
devludaley <daleyluis@gmail.com>
2021-02-14 17:23:41 -0500

    add new POST call to routine/:id//workouts

--- src/actions/addRoutineWorkout.js
+++ src/actions/addRoutineWorkout.js
@@ -4,18 +4,23 @@ console.log('🚀 ~ file: addRoutineWorkout.js ~ line 2 ~ addRoutineWorkout ~ da
 
     return(dispatch) => {
 // fetch(`http://localhost:3000/api/v1/routines/${data.id}`, {
-fetch(`https://be-workout-hero2.herokuapp.com/api/v1/routines/${data.id}`, {
+// fetch(`https://be-workout-hero2.herokuapp.com/api/v1/routines/${data.id}`, {
+fetch(`https://be-workout-hero2.herokuapp.com/api/v1/routines/${data.routineId}/workouts`, {
     headers: {
         'Content-Type' : 'application/json',
         'Accept' : 'application/json'
     },
-    method: 'PATCH',
+    method: 'POST',
     body: JSON.stringify(data)
 })
 .then(res => res.json())
+// .then(response => console.log('🚀 ~ file: addRoutineWorkout.js ~ line 18 ~ return ~ response', response)
+// )
 .then(routine => dispatch({
     type: 'ADD_ROUTINE_WORKOUT', 
     payload: routine
-}))
+}
+)
+)
     }
 };
