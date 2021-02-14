4275deace5ffd3202eef81cc3f4292f281214c14
devludaley <daleyluis@gmail.com>
2021-02-13 20:28:32 -0500

    change from rails-heroku to rails-local-api

--- src/actions/deleteRoutine.js
+++ src/actions/deleteRoutine.js
@@ -2,8 +2,8 @@ export const deleteRoutine = (routineData) => {
 
     console.log('🚀 ~ file: deleteRoutine.js ~ line 2 ~ deleteRoutine ~ routineData', routineData);
 
-    // const baseUrl = 'http://localhost:3000/api/v1/routines/'
-    const baseUrl = 'https://be-workout-hero2.herokuapp.com/api/v1/routines/'
+    const baseUrl = 'http://localhost:3000/api/v1/routines/'
+    // const baseUrl = 'https://be-workout-hero2.herokuapp.com/api/v1/routines/'
     const deleteRoutineUrl = (baseUrl + routineData.id)
 
     return (dispatch) => {
