7e21c81e335d561df7c293b989440b1be2c0de94
DevLuDaley <DaleyLuis@gmail.com>
2021-03-16 16:12:54 -0400

    remove unused import => Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './App.css';
 import MainContainer from './containers/MainContainer'
 import { Grid, Container } from '@material-ui/core';
