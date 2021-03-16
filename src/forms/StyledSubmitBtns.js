3900021ae12094271a0d542294b461618e756749
DevLuDaley <DaleyLuis@gmail.com>
2021-03-16 16:14:23 -0400

    src/forms/StyledSubmitBtns.js =>  add lineHeight

--- src/forms/StyledSubmitBtns.js
+++ src/forms/StyledSubmitBtns.js
@@ -14,6 +14,7 @@ const StyledSubmitBtns = () => {
 //   fontSize: '12px',
 //   padding: '10px 24px',
   borderRadius: '12px',
+  lineHeight: '2.6'
 //   transitionDuration: '0.4s',
 //   transition: 'background 0.4s ease-in-out',
     // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
