b24a0081747a8ad808ad3a37e4a7d1e409840d6c
DevLuDaley <DaleyLuis@gmail.com>
2021-03-16 16:13:19 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+const Header = () => {
+    return (
+        <>
+        <div class="header">
+          <a href="/" class="logo">Work Hero App</a>
+          <div class="header-right">
+            {/* <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a> */}
+          </div>
+        </div>
+        </>
+      );
+}
+export default Header;
