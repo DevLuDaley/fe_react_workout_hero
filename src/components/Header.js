a30c7f804fd7e09afc397876606034d525f2e505
DevLuDaley <DaleyLuis@gmail.com>
2021-03-17 12:18:56 -0400

    add ModalNewRoutineForm to <Header/>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ModalNewRoutineForm from '../forms/ModalNewRoutineForm'
 const Header = () => {
     return (
         <>
-        <div class="header">
-          <a href="/" class="logo">Work Hero App</a>
-          <div class="header-right">
+        <div className="header">
+          <a href="/" className="logo">Workout Hero App</a>
+          <div className="header-right">
             {/* <a class="active" href="#home">Home</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a> */}
+          <ModalNewRoutineForm/>
           </div>
         </div>
         </>
