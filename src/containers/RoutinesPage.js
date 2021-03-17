483d6827e23f5d35155cbb6fccdf38115a2d9139
DevLuDaley <DaleyLuis@gmail.com>
2021-03-16 20:31:47 -0400

    import ModalNewRoutineForm.js

--- src/containers/RoutinesPage.js
+++ src/containers/RoutinesPage.js
@@ -5,7 +5,9 @@ import Container from '@material-ui/core/Container';
 
 import RoutinesList from '../components/RoutinesList';
 import NewRoutineForm from '../forms/NewRoutineForm';
-
+// import Popup from 'reactjs-popup';
+import ModalNewRoutineForm from '../forms/ModalNewRoutineForm'
+import 'reactjs-popup/dist/index.css';
 
 
 class RoutinesPage extends Component {
@@ -15,23 +17,34 @@ class RoutinesPage extends Component {
         this.state = {routines: [], workouts: []}
     }
 
-  render() { 
+  render() {
             return (
             <>
                 {/* <OldRoutineForm routines={this.props.routines}/> */}
-    <Container maxWidth="lg" id="comp-routines-page">
-        <NewRoutineForm routines={this.props.routines}/>
-    {
-        this.props.routines && this.props.workouts ?
-            <Container maxWidth="lg" id="routines-list">
-                <RoutinesList routines={this.props.routines} workouts={this.props.workouts}/>
-            </Container> 
-        : null 
-    }
-    </Container>
+        <Container maxWidth="lg" id="comp-routines-page">
+            <ModalNewRoutineForm/>
+        {/* <Popup
+            trigger={
+                <button> Trigger</button>
+                }
+            position="left center"
+            >
+                <div>
+                <NewRoutineForm routines={this.props.routines}/>
+                </div>
+        </Popup> */}
+
+            {/* <NewRoutineForm routines={this.props.routines}/> */}
+        {
+            this.props.routines && this.props.workouts ?
+                <Container maxWidth="lg" id="routines-list">
+                    <RoutinesList routines={this.props.routines} workouts={this.props.workouts}/>
+                </Container>
+            : null
+        }
+        </Container>
             </>
             );
     }
 }
- 
 export default RoutinesPage;
