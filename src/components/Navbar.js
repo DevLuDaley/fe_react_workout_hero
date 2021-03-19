17d83967de0617e35e8fb50f07ece11ad1de2f96
DevLuDaley <DaleyLuis@gmail.com>
2021-03-19 10:35:33 -0400

    move ModalNewRoutineForm from Header.js to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Link } from 'react-router-dom'
+
 import ModalNewRoutineForm from '../forms/ModalNewRoutineForm'
-const Header = () => {
+
+
+const Navbar = () => {
     return (
         <>
-        <div className="header">
-          <a href="/" className="logo">Workout Hero App</a>
+        <div className="navbar">
+          <a href="/" className="logo"></a>
           <div className="header-right">
             {/* <a class="active" href="#home">Home</a>
             <a href="#contact">Contact</a>
@@ -20,12 +23,18 @@ const Header = () => {
                 </Link>
             </Switch> */}
 
-
-
-          {/* <ModalNewRoutineForm/> */}
+ <Switch>
+                <Link 
+                    to={'/routines'}>
+                    <button 
+                      className='floating-nav-btns'> Routines 
+                    </button>
+                </Link>
+            </Switch>
+          <ModalNewRoutineForm/>
           </div>
         </div>
         </>
       );
 }
-export default Header;
+export default Navbar;
