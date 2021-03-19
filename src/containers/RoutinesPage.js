5a7a50089e9fad8f6b700593788506db4a0dd962
DevLuDaley <DaleyLuis@gmail.com>
2021-03-19 10:36:27 -0400

    RoutinesPage.js => comment out css import

--- src/containers/RoutinesPage.js
+++ src/containers/RoutinesPage.js
@@ -7,7 +7,7 @@ import RoutinesList from '../components/RoutinesList';
 import NewRoutineForm from '../forms/NewRoutineForm';
 // import Popup from 'reactjs-popup';
 import ModalNewRoutineForm from '../forms/ModalNewRoutineForm'
-import 'reactjs-popup/dist/index.css';
+// import 'reactjs-popup/dist/index.css';
 
 
 class RoutinesPage extends Component {
@@ -20,29 +20,32 @@ class RoutinesPage extends Component {
   render() {
             return (
             <>
-                {/* <OldRoutineForm routines={this.props.routines}/> */}
-        <Container maxWidth="lg" id="comp-routines-page">
-            {/* <ModalNewRoutineForm/> */}
-        {/* <Popup
-            trigger={
-                <button> Trigger</button>
-                }
-            position="left center"
-            >
-                <div>
-                <NewRoutineForm routines={this.props.routines}/>
-                </div>
-        </Popup> */}
+                    {/* <OldRoutineForm routines={this.props.routines}/> */}
+                <Container maxWidth="lg" id="component-routines-page">
+                    {/* <ModalNewRoutineForm/> */}
+                    {/* <Popup
+                    trigger={
+                        <button> Trigger</button>
+                        }
+                    position="left center"
+                    >
+                        <div>
+                        <NewRoutineForm routines={this.props.routines}/>
+                        </div>
+                    </Popup> */}
 
-            {/* <NewRoutineForm routines={this.props.routines}/> */}
-        {
-            this.props.routines && this.props.workouts ?
-                <Container maxWidth="lg" id="routines-list">
-                    <RoutinesList routines={this.props.routines} workouts={this.props.workouts}/>
+                    {/* <NewRoutineForm routines={this.props.routines}/> */}
+
+               <div id='container-all-routines'>
+                    {
+                        this.props.routines && this.props.workouts ?
+                            <Container maxWidth="lg" id="routines-list">
+                                <RoutinesList routines={this.props.routines} workouts={this.props.workouts}/>
+                            </Container>
+                        : null
+                    }
+               </div>
                 </Container>
-            : null
-        }
-        </Container>
             </>
             );
     }
