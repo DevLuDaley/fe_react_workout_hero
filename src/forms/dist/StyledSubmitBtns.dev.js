73d3b27e50337dc15fdf67769e989da5614a76d7
DevLuDaley <DaleyLuis@gmail.com>
2021-03-20 20:17:14 -0400

    style popup/modal window

---
+++ src/forms/dist/StyledSubmitBtns.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var StyledSubmitBtns = function StyledSubmitBtns() {
+  return {
+    root: {
+      //   backgroundColor: 'rgba(139, 0, 139, 0.507)',
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      //   border: 'none',
+      color: 'white',
+      //   padding: '15px 32px',
+      //   textAlign: 'center',
+      textDecoration: 'none',
+      display: 'inline-block',
+      fontSize: '12px',
+      //   padding: '10px 24px',
+      borderRadius: '12px',
+      lineHeight: '2.6',
+      textTransform: "capitalize" //   transitionDuration: '0.4s',
+      //   transition: 'background 0.4s ease-in-out',
+      // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      // borderRadius: 3,
+      // border: 0,
+      // color: 'white',
+      // height: 48,
+      // padding: '0 30px',
+      // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+
+    }
+  };
+};
+
+var _default = StyledSubmitBtns;
+exports["default"] = _default;
